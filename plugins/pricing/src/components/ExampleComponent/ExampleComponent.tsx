3c591d9e3bdd0969e8e9b1baf5c014e8eb1f1b8c
tejakoheda <kohedaraviteja@gmail.com>
2026-02-26 15:32:38 +0530

    second commit

--- plugins/pricing/src/components/ExampleComponent/ExampleComponent.tsx
+++ plugins/pricing/src/components/ExampleComponent/ExampleComponent.tsx
@@ -1,3 +1,50 @@
+import DataTable from 'react-data-table-component';
 export const ExampleComponent = () => {
-  return;
+  const columns = [
+    {
+      name: 'Title',
+      selector: (row: any) => row.title,
+    },
+    {
+      name: 'Year',
+      selector: (row: any) => row.year,
+      sortable: true,
+    },
+  ];
+  const data = [
+    {
+      id: 1,
+      title: 'Beetlejuice',
+      year: '1988',
+    },
+    {
+      id: 2,
+      title: 'Ghostbusters',
+      year: '1984',
+    },
+  ];
+  return (
+    <div className="price">
+      <h1 className="Top-Data">Price Management</h1>
+      <div className="main">
+        <div className="select-add">
+          <div className="Selection">
+            <select>
+              <option>Select</option>
+              <option>Flat</option>
+              <option>slab</option>
+              <option>DayWise</option>
+            </select>
+          </div>
+          <div className="add">
+            <button>Add Price</button>
+          </div>
+        </div>
+        <hr />
+        <div className="table">
+          <DataTable columns={columns} data={data} />
+        </div>
+      </div>
+    </div>
+  );
 };
